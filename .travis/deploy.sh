44b9e7370cc2d3e59a35050d194b68989191760b
Datenpunk <32763831+dtnpnk@users.noreply.github.com>
2018-03-12 20:17:56 +0100

    Update deploy.sh

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
-GH_USER="dtnpnk"
-GH_REPO="docs-gitbook"
 
 ls -lha
 
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis"
-git clone --quiet --branch=gh-pages https://${GITHUB_TOKEN}@github.com/${GH_USER}/${GH_REPO}.git gh-pages > /dev/null
+git clone --quiet --branch=gh-pages https://${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git gh-pages > /dev/null
 
 cd gh-pages
 cp -Rf $TRAVIS_BUILD_DIR/_book/* .   
